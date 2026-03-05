502b92a855d5169563c9bc3e5c5cb0ea88774431
keevega <keevega01@gmail.com>
2026-03-05 13:53:23 -0600

    Model User Created with sequelize and db connected using .env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 import Sequelize from "sequelize";
+import dotenv from 'dotenv'
+dotenv.config({path: '.env'})
 
-const db = new Sequelize('bienesraices_node_mvc', 'root', 'kevin970701', {
-    host: 'localhost',
+const db = new Sequelize(process.env.DB_NOMBRE, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     port: 3306,
     dialect: 'mysql',
     define: {
