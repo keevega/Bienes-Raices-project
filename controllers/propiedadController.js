96cfffee219a39436715b9068606ca96ce9ee7b6
keevega <keevega01@gmail.com>
2026-03-12 13:56:14 -0600

    Adding forms to create-property

---
+++ controllers/propiedadController.js
@@ -0,0 +1,22 @@
+
+
+const admin = (req, res) => {
+    res.render('propiedades/admin',{
+        pagina: 'Mis Propiedades',
+        barra: true
+    })
+}
+
+
+//Formulario para crear una nueva propiedad
+const crear = (req, res) => {
+    res.render('propiedades/crear',{
+        pagina: 'Crear Propiedad',
+        barra: true
+    })
+}
+
+export {
+    admin,
+    crear
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import csrf from 'csurf'
 import cookieParser  from 'cookie-parser'
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import propiedadesRoutes from './routes/propiedadesRoutes.js'
 import db from './config/db.js'
 
 // Crear la app
@@ -42,6 +43,7 @@ app.use( express.static('public'));
 //Routing (endpoints)
 
 app.use('/auth', usuarioRoutes);
+app.use('/', propiedadesRoutes);
 
 
 
